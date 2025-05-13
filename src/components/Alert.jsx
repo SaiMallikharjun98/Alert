f4e1240f2c64fb8b4edd7c7ce4b61e18308edcc1
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-13 13:08:17 +0530

    Padding changes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -30,15 +30,15 @@ function Alert() {
             </div>
           )}
         </button>
-        <div className="relative">
-          {alertStatus ? (
-            <div className="w-[200px] absolute left-120 bottom-20 text-xl rounded-lg  border-gray-200 border-[0.5px] p-2 ">
-              🚨 Show Warning
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
+      </div>
+      <div className="relative">
+        {alertStatus ? (
+          <div className="w-[200px] absolute left-120 bottom-20 text-xl rounded-lg  border-gray-200 border-[0.5px] p-2 ">
+            🚨 Show Warning
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
