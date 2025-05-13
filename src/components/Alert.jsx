e4dc59d6ba5d4aa464958f48f3c4600787a8971f
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-13 12:41:58 +0530

    Padding changes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -28,7 +28,7 @@ function Alert() {
         </div>
         <button onClick={alertHandler}>
           {alertStatus ? (
-            <div className="text-2xl bg-violet-300 hover:bg-violet-700 text-white cursor-pointer mt-8 p-6">
+            <div className="text-2xl bg-violet-300 hover:bg-violet-700 text-white cursor-pointer mt-8 px-6 pt-3">
               Hide Alert
             </div>
           ) : (
