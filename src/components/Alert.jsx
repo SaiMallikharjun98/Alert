350cf8bfb4c67c00f9bcf0bda8dffdd67e443808
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-13 13:07:06 +0530

    Padding changes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -9,23 +9,16 @@ function Alert() {
     <>
       <div className="mx-auto max-w-md">
         <div className="px-5 py-20">
-          <h1 className="text-3xl font-bold pb-2">This is an Simple Alert app</h1>
+          <h1 className="text-3xl font-bold pb-2">
+            This is an Simple Alert app
+          </h1>
           <p className="tet-2xl">
             The Alert App is a simple React application that demonstrates the
             use of React's useState hook for managing component state
             interactively.
           </p>
         </div>
-        <div className="relative">
-          {alertStatus ? (
-            <div className="w-[200px] absolute left-120 bottom-20 text-xl rounded-lg  border-gray-200 border-[0.5px] p-2 ">
-              
-              🚨 Show Warning
-            </div>
-          ) : (
-            ""
-          )}
-        </div>
+
         <button onClick={alertHandler}>
           {alertStatus ? (
             <div className="text-2xl bg-violet-300 hover:bg-violet-700 text-white cursor-pointer p-5">
@@ -37,6 +30,15 @@ function Alert() {
             </div>
           )}
         </button>
+        <div className="relative">
+          {alertStatus ? (
+            <div className="w-[200px] absolute left-120 bottom-20 text-xl rounded-lg  border-gray-200 border-[0.5px] p-2 ">
+              🚨 Show Warning
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </>
   );
