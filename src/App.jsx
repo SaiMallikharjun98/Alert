9f61f448e8a180634957d352d3d8516443634b62
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-13 12:27:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Alert from "./components/Alert";
+function App() {
+  return (
+    <>
+      <Alert />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Alert.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function Alert() {
+  let [alertStatus, setAlertStatus] = useState(false);
+  const alertHandler = () => {
+    setAlertStatus((prev) => !prev);
+  };
+  return (
+    <>
+      <div className="mx-auto max-w-md">
+        <div className="px-10">
+          <h1 className="text-3xl font-bold ">This is an Simple Alert app</h1>
+          <p className="tet-2xl">
+            The Alert App is a simple React application that demonstrates the
+            use of React's useState hook for managing component state
+            interactively.
+          </p>
+          <div className="relative">
+          {alertStatus ? <div className="absolute left-50 bottom-40 text-sm rounded-lg  border-gray-200 border-[0.5px] "> 🚨 Show Warning </div> : ""}
+          </div>
+          <button onClick={alertHandler}>
+            {alertStatus ? (
+              <div className="text-2xl bg-violet-300 hover:bg-violet-700 text-white cursor-pointer mt-8 p-6">
+                Hide Alert
+              </div>
+            ) : (
+              <div className="text-2xl bg-violet-300 hover:bg-violet-700 text-white cursor-pointer mt-8 p-6">
+                Show Alert
+              </div>
+            )}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Alert;
